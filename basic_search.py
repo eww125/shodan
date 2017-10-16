6f13d2b6b8ffcd8c37e3493b31fd5f69092efdda
Evan Wappel <ewappel@gmail.com>
2017-10-16 11:56:05 -0700

    Update basic_search.py

--- basic_search.py
+++ basic_search.py
@@ -1,110 +1,29 @@
-#!/usr/bin/env python
-#
-# query-summary.py
-# Search Shodan and print summary information for the query.
-#
-# Author: achillean
-
 import shodan
-import sys
 
 # Configuration
-API_KEY = 'your-API-key'
+import os
+home_dir =  os.path.expanduser('~')
+f = open(home_dir + '/api_keys/shodan_api', 'r')
+API_KEY = f.readline().splitlines()[0]
+print API_KEY
 
-# The list of properties we want summary information on
-FACETS = [
-    'org',
-    'domain',
-    'port',
-    'asn',
+api = shodan.Shodan(API_KEY)
 
-    # We only care about the top 3 countries, this is how we let Shodan know to return 3 instead of the
-    # default 5 for a facet. If you want to see more than 5, you could do ('country', 1000) for example
-    # to see the top 1,000 countries for a search query.
-    ('country', 3),
-]
-
-FACET_TITLES = {
-    'org': 'Top 5 Organizations',
-    'domain': 'Top 5 Domains',
-    'port': 'Top 5 Ports',
-    'asn': 'Top 5 Autonomous Systems',
-    'country': 'Top 3 Countries',
-}
-
-# Input validation
-if len(sys.argv) == 1:
-    print 'Usage: %s <search query>' % sys.argv[0]
-    sys.exit(1)
-
+# Wrap the request in a try/ except block to catch errors
 try:
-    # Setup the api
-    api = shodan.Shodan(API_KEY)
+        # Search Shodan
+        results = api.search('apache')
 
-    # Generate a query string out of the command-line arguments
-    query = ' '.join(sys.argv[1:])
-
-    # Use the count() method because it doesn't return results and doesn't require a paid API plan
-    # And it also runs faster than doing a search().
-    result = api.count(query, facets=FACETS)
-
-    print 'Shodan Summary Information'
-    print 'Query: %s' % query
-    print 'Total Results: %s\n' % result['total']
-
-    # Print the summary info from the facets
-    for facet in result['facets']:
-        print FACET_TITLES[facet]
-
-        for term in result['facets'][facet]:
-            print '%s: %s' % (term['value'], term['count'])
-
-        # Print an empty line between summary info
-        print ''
-
-except Exception, e:
-    print 'Error: %s' % e
-    sys.exit(1)
-
-"""
-Sample Output
-=============
-
-./query-summary.py apache
-Shodan Summary Information
-Query: apache
-Total Results: 34612043
-
-Top 5 Organizations
-Amazon.com: 808061
-Ecommerce Corporation: 788704
-Verio Web Hosting: 760112
-Unified Layer: 627827
-GoDaddy.com, LLC: 567004
-
-Top 5 Domains
-secureserver.net: 562047
-unifiedlayer.com: 494399
-t-ipconnect.de: 385792
-netart.pl: 194817
-wanadoo.fr: 151925
-
-Top 5 Ports
-80: 24118703
-443: 8330932
-8080: 1479050
-81: 359025
-8443: 231441
-
-Top 5 Autonomous Systems
-as32392: 580002
-as2914: 465786
-as26496: 414998
-as48030: 332000
-as8560: 255774
-
-Top 3 Countries
-US: 13227366
-DE: 2900530
-JP: 2014506
-"""
+        # Show the results
+        print 'Results found: %s' % results['total']
+        x = 1
+        for result in results['matches']:
+                print "x=" + str(x)
+                print 'IP: %s' % result['ip_str']
+                print result['data']
+                print ''
+                x = x + 1
+                if x == 5:
+                        break
+except shodan.APIError, e:
+        print 'Error: %s' % e
