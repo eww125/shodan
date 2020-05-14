6ed1924354c405a7f59073f33dd6af7ad2320765
Evan Wappel <ewappel@gmail.com>
2020-05-13 19:09:11 -0700

    Update basic_search.py

--- basic_search.py
+++ basic_search.py
@@ -5,7 +5,7 @@ import os
 home_dir =  os.path.expanduser('~')
 f = open(home_dir + '/api_keys/shodan_api', 'r')
 API_KEY = f.readline().splitlines()[0]
-print API_KEY
+print(API_KEY)
 
 api = shodan.Shodan(API_KEY)
 
@@ -15,15 +15,15 @@ try:
         results = api.search('apache')
 
         # Show the results
-        print 'Results found: %s' % results['total']
+        print('Results found: %s' % results['total'])
         x = 1
         for result in results['matches']:
-                print "x=" + str(x)
-                print 'IP: %s' % result['ip_str']
-                print result['data']
-                print ''
+                print("x=" + str(x))
+                print('IP: %s' % result['ip_str'])
+                print(result['data'])
+                print('')
                 x = x + 1
                 if x == 5:
                         break
 except shodan.APIError, e:
-        print 'Error: %s' % e
+        print('Error: %s' % e)
